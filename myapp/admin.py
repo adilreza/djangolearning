bbac4cf7457499c676034aca39ce30416cf5b220
Adil Reza <adilreza043@gmail.com>
2020-07-17 21:20:28 +0600

    adding new table name myfirsttable

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
+# imported all models
+
+from .models import MyFirstTable
+
 # Register your models here.
+
+
+admin.site.register(MyFirstTable)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MyFirstTable(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(unique=True, max_length=150)
+    age = models.PositiveSmallIntegerField()
+    address = models.TextField(max_length=3200000)
+    is_activate = models.BooleanField()
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.email
