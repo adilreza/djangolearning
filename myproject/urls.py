eeced834c79d43e2e438af6b611cfd1672d09c59
Adil Reza <adilreza043@gmail.com>
2020-07-19 22:12:34 +0600

    we have done update , delete and retrive option in post table

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from myapp.views import index_file
 from myapp.views import adil_file, charlotte_file, newpage
-from myapp.views import about, contact, my_form, my_form2
+from myapp.views import about, contact, my_form, my_form2, manage_data, delete_post, edit_post, update_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,9 @@ urlpatterns = [
     path('post/', my_form),
     path('', charlotte_file),
     path('form2/', my_form2),
+    path('manage_data/', manage_data),
+    path('delete_post/<int:id>', delete_post),
+    path('edit_post/<int:id>', edit_post),
+    path('update_post/', update_post),
 
 ]
