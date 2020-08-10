7df02372f7704eb720dc2d294d8978f7e5cbda34
Adil Reza <adilreza043@gmail.com>
2020-08-10 21:28:52 +0600

    adding postgresql to djagno

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
+
 
 # Create your models here.
 
@@ -60,3 +62,12 @@ class CharlotteAjaxTable(models.Model):
     color = models.CharField(max_length=100)
     def __str__(self):
         return self.name
+
+
+class AdvancedDataType(models.Model):
+    name = models.CharField(max_length=30)
+    json_data = JSONField()
+
+    def __str__(self):
+        return self.name
+
