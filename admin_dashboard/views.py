27c86084bb37cedf4f2b399faf41b985d8def5ba
Adil Reza <adilreza043@gmail.com>
2020-08-29 20:15:42 +0600

    solving the login issue

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -5,20 +5,28 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
 def user_login(request):
     if request.method == "GET":
         return render(request, 'admin_dash/login.html', {'success': True})
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+
+        print(username)
+        print(password)
+
         user = authenticate(username=username, password=password)
 
         if user is not None:
             login(request,user)
             request.session['my_auth_user']=username
-            return render(request, 'admin_dash/todo.html', {'success':True})
+            return render(request, 'admin_dash/index.html', {'success':True})
         else:
             return render(request, 'admin_dash/login.html', {'success': False})
 
+
 def home(request):
     return render(request, 'admin_dash/index.html')
+
